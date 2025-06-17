692e363bf0e4f8c76d9123344bf5024315289f85
Koul <dongik0122@gmail.com>
2025-06-17 20:08:29 +0900

    [#3] 샘플모델 생성

--- djangoProject/user_app/models.py
+++ djangoProject/user_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longtitude = models.DecimalField(max_digits=9, decimal_places=6)
+    rating = models.SmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
